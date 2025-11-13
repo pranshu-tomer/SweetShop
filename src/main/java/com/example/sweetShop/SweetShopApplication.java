5730e97d91bdf37b642a939cd13c8757985a75de
pranshu-tomer <tomerpranshu11@gmail.com>
2025-11-14 00:21:50 +0530

    Basic Project Setup Complete

--- src/main/java/com/example/sweetShop/SweetShopApplication.java
+++ src/main/java/com/example/sweetShop/SweetShopApplication.java
@@ -12,4 +12,14 @@ public class SweetShopApplication {
 
 }
 
-//
+// 3 = done
+// 4 = done
+// 5 = done
+// 6 = done
+// 7 = done
+// 8 = done
+// 9 = done
+// 10 = done
+// 11 = done
+// 12 = done
+// 13 = done  username = admin, password = adminpass
